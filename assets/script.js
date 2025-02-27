6f5258f3b3f64ef9d33a513f1edc403000b39131
Samuel Korosi <samuel.korosi@gmail.com>
2025-02-27 20:25:45 -0300

    Adicionado efeito "flip" ao obter cupom ou desvirar card

--- assets/script.js
+++ assets/script.js
@@ -6,23 +6,17 @@ const btnBack = document.getElementById("btn-back");
 const btnCopy = document.getElementById("btn-copy");
 const cupomTxt = document.getElementById("cupom");
 
+const flipCard = document.getElementById("flip-card");
+
 btnObter.addEventListener("click", x => {
     console.log("Obter cupom!");
 
-    console.log("Front:");
-    console.log(cardFront);
-    console.log("Back:");
-    console.log(cardBack);
-
-    cardFront.classList.add("hidden");
-    cardBack.classList.remove("hidden");
+    flipCard.classList.add("clicked");
 });
 
 btnBack.addEventListener("click", x => {
     console.log("Usar cupom!");
-
-    cardFront.classList.remove("hidden");
-    cardBack.classList.add("hidden");
+    flipCard.classList.remove("clicked");
 });
 
 btnCopy.addEventListener("click", x => {
